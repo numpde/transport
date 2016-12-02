7f13440ac38ab877b2e849f21866cdb8b310142b
andreevr <romanize@hotmail.com>
2016-12-02 19:45:02 +0100

    RA: Another (dummy) AI

---
+++ cha01/ai_simul.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import random as rnd
+
+class navigator :
+	name = "Simul"
+	
+	def __init__(self, C, N) :
+		self.C = C
+		self.N = N
+	#/def
+
+	def step(self, b, B, G) :
+		# Number of passengers to board
+		z = min(len(G[b]), self.C - len(B))
+		# Passenger selection
+		M = [m for m in range(z)]
+		
+		return M, rnd.choice([-1, +1])
+	#/def
+#/class
+
