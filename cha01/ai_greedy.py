520657f00160e4c6db1e0532569e2260c5ba336f
andreevr <romanize@hotmail.com>
2016-12-03 20:35:15 +0100

    RA: Various changes

--- cha01/ai_greedy.py
+++ cha01/ai_greedy.py
@@ -2,8 +2,10 @@
 
 class navigator :
 	name = "Greedy"
-	
+
 	def __init__(self, C, N) :
+		assert isinstance(C, int) and (C >= 1)
+		assert isinstance(N, int) and (N >= 1)
 		# Capacity of the bus (integer >= 1)
 		self.C = C
 		# Number of stations (integer >= 1)
@@ -11,59 +13,101 @@ class navigator :
 
 	def step(self, b, B, Q) :
 		# INPUT:
-		#
-		# b is an integer 0 <= b < N denoting
-		#   the current location of the bus.
-		#
-		# B is a list [b1, b2, ..] of
-		#   passengers currently on the bus
-		#   (not exceeding the capacity), where
-		#   bk is the destination of passenger k.
-		#   The order is that of boarding
-		#   (provided by this function: see M).
-		#
-		# Q is a list of N lists, where
-		#   Q[n] = [t1, t2, ..] is the list of
-		#   people currently waiting at station n
-		#   with destinations t1, t2, ..
+		# See check_input(..) for details.
+		# The function may not modify b, B or Q.
 		#
 		# OUTPUT:
-		#
-		# return M, s where
-		#
-		# M is a list of indices M = [i1, i2, .., im]
-		#   into the list Q[b] indicating that
-		#   the people Q[b][i] will board the bus
-		#   (in the order defined by M). 
-		#   Set M = [] if no one boards the bus.
-		#   Note the constraints:
-		#     len(B) + len(M) <= Capacity C, 
-		#   and
-		#     0 <= i < len(Q[b]) for each i in M.
-		#
-		# s is either +1, -1, or 0, indicating
-		#   the direction of travel of the bus
-		#   (the next station is (b + s) % N).
+		# The function should return M, s
+		# See check_output(..) for details.
 
-	
+		self.check_input(b, B, Q)
+
 		# Number of passengers to board
 		n = min(len(Q[b]), self.C - len(B))
 		# Passenger selection from Q[b]
 		M = list(range(n))
-		
+
 		# No passengers?
 		if ((not B) and (not M)) : return [], 1
-		
+
 		# Next passenger's destination
 		t = (B + [Q[b][i] for i in M])[0]
-		
+
 		# Destination relative to current position
 		t = (self.N/2 - ((t-b) % self.N))
-		
+
 		# Move towards that destination
 		s = (+1) if (t > 0) else (-1)
-		
+
+		self.check_output(b, B, Q, M, s)
 		return M, s
+
+
+	def is_station(self, n) :
+		return isinstance(n, int) and ((0 <= n) and (n < self.N))
+
+	def check_input(self, b, B, Q) :
+		#
+		# 1.
+		# b is an integer 0 <= b < N denoting
+		#   the current location of the bus.
+		#
+		assert self.is_station(b)
+		#
+		# 2.
+		# B is a list [n1, n2, ..] of
+		#   the destinations of the passengers
+		#   currently on the bus
+		#   (not exceeding the capacity), i.e.
+		#   nk is the destination of passenger k.
+		#   The order is that of boarding
+		#   (provided by this function: see M).
+		#   No destination is the current position.
+		#
+		assert isinstance(B, list)
+		assert all(self.is_station(n) for n in B)
+		assert all((n != b) for n in B)
+		#
+		# 3.
+		# Q is a list of N lists, where
+		#   Q[n] = [t1, t2, ..] is the list of
+		#   people currently waiting at station n
+		#   with destinations t1, t2, ..
+		#   No destination equals the location,
+		#   i.e. (t != n) for any t in Q[n]
+		#
+		assert isinstance(Q, list)
+		assert (len(Q) == self.N)
+		assert all(isinstance(q, list) for q in Q)
+		assert all(all(self.is_station(t) for t in q) for q in Q)
+		assert all(all((t != n) for t in q) for n, q in enumerate(Q))
+
+	def check_output(self, b, B, Q, M, s) :
+		#
+		# 1.
+		# M is a list of indices M = [i1, i2, .., im]
+		#   into the list Q[b] indicating that
+		#   the people Q[b][i] will board the bus
+		#   (in the order defined by M).
+		#   Set M = [] if no one boards the bus.
+		#   Note the constraints:
+		#     len(B) + len(M) <= Capacity C,
+		#   and
+		#     0 <= i < len(Q[b]) for each i in M.
+		#
+		assert isinstance(M, list)
+		assert all(isinstance(i, int) for i in M)
+		assert all(((0 <= i) and (i < len(Q[b]))) for i in M)
+		assert (len(B) + len(M) <= self.C)
+		#
+		# 2.
+		# s is either +1, -1, or 0, indicating
+		#   the direction of travel of the bus
+		#   (the next station is (b + s) % N).
+		#
+		assert isinstance(s, int)
+		assert (s in [-1, 0, 1])
+
 
 
 
--- cha01/fw.py
+++ cha01/fw.py
@@ -2,8 +2,6 @@
 
 # R. Andreev, 2016-12-03
 
-# Competing navigators
-import ai_clock, ai_klock, ai_greedy, ai_error, ai_simul
 
 # Bus capacity
 C = 3
@@ -11,79 +9,17 @@ C = 3
 N = 6
 
 
-import random
+from world import World
+from numpy import mean
 #
-class World :
-
-	def __init__(self, C, N) :
-		self.C = C     # Bus capacity
-		self.N = N     # Number of stations
-		self.b = None  # Bus position
-		self.B = None  # Bus passengers' destinations [list]
-		self.Q = None  # Queues at stations [list of list]
-		self.i = None  # Iteration number (time)
-		self.NEWS = [None] # World trajectory record [list of tuple/None]
-		self.rewind()
-
-	def rewind(self) :
-		self.b = 0
-		self.B = []
-		self.Q = [[] for _ in range(self.N)]
-		self.i = 0
-
-	def news(self) :
-		# Create news if necessary
-		if (len(self.NEWS) <= self.i) :
-			# New person arrives at "a" with destination "b"
-			a = random.randint(0, self.N-1)
-			b = (a + random.randint(1, self.N-1)) % self.N
-			self.NEWS.append((a, b))
-		assert((0 <= self.i) and (self.i < len(self.NEWS)))
-		return self.NEWS[self.i]
-
-	def look(self) :
-		return self.b, self.B, self.Q
-
-	def move(self, M, s) :
-		self.check_suggestion(M, s)
-
-		# Passengers mount (in the given order)
-		for m in M : self.B.append(self.Q[self.b][m])
-		# Remove them from the queue
-		for m in sorted(M, reverse=True) : self.Q[self.b].pop(m)
-
-		# Advance bus
-		self.b = (self.b + s) % self.N
-
-		# Passengers unmount
-		self.B = [p for p in self.B if (p != self.b)]
-
-		# Advance time
-		self.i += 1
-
-		assert (self.news() is not None)
-		# New person arrives at "a" with destination "b"
-		a, b = self.news()
-		# Queue in the new person
-		self.Q[a].append(b)
-
-	def check_suggestion(self, M, s) :
-		assert isinstance(M, (list)), "M should be a list (of indices)"
-		assert (s in [-1, 0, 1]), "s should be a sign: -1, +1 or 0"
-		assert (len(M) + len(self.B) <= self.C), "Exceeded the bus capacity"
-
-	def get_w(self) :
-		# Number of people waiting in queue
-		return sum(len(P) for P in self.Q)
-
-
-import numpy as np
 class Profiler :
 	def __init__(self, wrd, nav, I) :
-		self.W = []   # W[i] = people waiting at time i
-		self.w = None # w = average
+		# W[i] = average number of people waiting at time i
+		self.W = []   
+		# w = average over time
+		self.w = None
 
-		assert((0 <= I) and (I <= 1e10))
+		assert((0 < I) and (I <= 1e9))
 
 		wrd.rewind()
 		assert(wrd.i == 0)
@@ -91,26 +27,34 @@ class Profiler :
 		while (wrd.i < I) :
 			wrd.move(*nav.step(*wrd.look()))
 			self.W.append(wrd.get_w())
-			
-		self.w = np.mean(self.W)
+		
+		assert(len(self.W))
+		self.w = mean(self.W)
 
 
 
-###
-print("1. Initializing navigators")
-###
-
-NAV = []
-import sys
 #
-for module in list(sys.modules) :
-	if not module.startswith('ai_') : continue
+print("1. Initializing navigators")
+
+# Competing navigators
+NAV = []
+
+# Load all local ai_* modules
+# Ref: http://stackoverflow.com/questions/8718885/
+from os.path import dirname, basename, isfile
+import glob, importlib, sys
+modules = glob.glob(dirname(__file__) + "/*.py")
+modules = [basename(f)[:-3] for f in modules if isfile(f)]
+#modules = list(set(modules)) # unique names
+modules = [m for m in modules if m.startswith("ai_")]
+for module in modules : 
 	try :
-		Nav = getattr(__import__(module), 'navigator')
+		m = importlib.import_module(module)
+		Nav = getattr(m, 'navigator')
 		NAV.append(Nav(C, N))
-		print(" - Module " + module + " ok")
+		print("Loaded module", module)
 	except Exception as err:
-		print(" - Module " + module + " failed:", err)
+		print("Loading module", module, "failed:", err)
 
 def get_name(nav) :
 	try :
@@ -120,18 +64,16 @@ def get_name(nav) :
 
 
 
-###
+#
 print("2. Profiling navigators")
-###
-
-# Number of iterations (time steps)
-I = 10000
 
 # Ranks
 R = [None for _ in NAV]
-
+# Scores
+S = [[] for _ in NAV]
 
 #import matplotlib.pyplot as plt
+from numpy import mean
 
 # While some ranks are undecided
 while [r for r in R if r is None] :
@@ -142,40 +84,45 @@ while [r for r in R if r is None] :
 	wrd = World(C, N)
 
 	# Navigator scores (nonnegative; max score loses)
-	S = [None for _ in NAV]
+	K = []
 	for n, nav in enumerate(NAV) :
 		if (R[n] is not None) : continue
 		
 		print(" - Profiling:", get_name(nav))
 		try :
-			# Default navigator score = +oo
-			S[n] = +np.inf
+			# Number of iterations (time steps)
+			I = 10000
 			# Profile the navigator on the world
 			report = Profiler(wrd, nav, I)
 			# Score = average number of people waiting
-			S[n] = report.w
-			#plt.plot(report.W)
+			K.append((n, report.w))
+			#plt.plot(report.W) # History
 		except Exception as err :
+			R[n] = rank
 			print("    - Error:", err)
-
-	#plt.show()
-
+	
+	
+	
 	# Rank the losers
-	maxS = max(s for s in S if s is not None)
-	for n, s in enumerate(S) :
-		if (s == maxS) : R[n] = rank
-
-	for n, s in enumerate(S) :
-		print("# ", rank, n, s, R[n])
+	for n, s in K :
+		if (s == max(s for n, s in K)) : R[n] = rank
+		S[n].append(mean(S[n] + [s]))
 
 
-
-###
+#
 print("3. Final ranking:")
-###
 
-for r in sorted(R) :
-	print(r, get_name(NAV[R.index(r)]))
+for r in sorted(list(set(R))) :
+	print(r, [get_name(NAV[i]) for i, rr in enumerate(R) if (r == rr)])
 
+
+import matplotlib.pyplot as plt
+for s in S :
+	plt.plot(s, '-x')
+plt.yscale('log')
+plt.xlabel('Round')
+plt.ylabel('Score (less is better)')
+plt.legend([get_name(nav) for nav in NAV], numpoints=1)
+plt.show()
 
 
