42b2799db0b8d6be20249bf39b943cdfcb2b2e4d
andreevr <romanize@hotmail.com>
2016-12-28 16:47:10 +0100

    RA: (Revision)

--- busybus/java/Main.java
+++ busybus/java/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 	
@@ -8,19 +9,18 @@ public class Main {
 	final static int N = 6;
 	
 	static class Score implements Comparable<Score> {
-		int constestant;
+		int contestant;
 		double score;
 		public Score(int contestant, double score) {
-			this.constestant = contestant;
+			this.contestant = contestant;
 			this.score = score;
 		}
 		
 		@Override
 		public int compareTo(Score o) {
-			double w = this.score - ((Score) o).score;
-			if (w < 0) return -1;
-			else if (w > 0) return 1;
-			else return 0;
+			double d = this.score - o.score;
+			// Return the sign of the difference d
+			return ((d > 0) ? 1 : (d < 0) ? -1 : 0);
 		}
 	}
 	
@@ -32,18 +32,25 @@ public class Main {
 		strategies.add(new AI_GREEDY(C, N));
 		
 		
+		// The following block computes for each contestant n:
+		// 1. The rank in ranks.get(n)
+		List<Integer> ranks = new ArrayList<>();
+		// 2. The score history in scores.get(n)
+		List<List<Double>> scores = new ArrayList<>();
+		
 		System.out.println("Starting competition");
-		int[] ranks = new int[strategies.size()];
 		{
-		
-			// Next rank to be assigned
-			int rank = ranks.length;
-		
-			List<List<Double>> scores = new ArrayList<>();
-			for (int i = 0; i < rank; i++)
-				scores.add(new ArrayList<>());
-		
-			while (rank > 0) {
+			
+			// Initialize ranks and scores
+			while (ranks.size() < strategies.size()) ranks.add(0);
+			while (scores.size() < strategies.size()) scores.add(new ArrayList<Double>());
+			
+				
+			// Next rank to be assigned is
+			// the number of currently unassigned ranks
+			int rank;
+			// Continue while that number is non-zero
+			while ((rank = Collections.frequency(ranks, 0)) > 0) {
 				System.out.println("Number of competitors: " + rank);
 			
 				World world = new World(C, N);
@@ -53,8 +60,9 @@ public class Main {
 				List<Score> K = new ArrayList<>();
 			
 				for (int n = 0; n < strategies.size(); ++n) {
-					if (ranks[n] > 0) continue;
-				
+					// Only contestants with unassigned rank participate
+					if (ranks.get(n) > 0) continue;
+					
 					Strategy strategy = strategies.get(n);
 				
 					System.out.println(" - Profiling " + strategy.name);
@@ -70,31 +78,28 @@ public class Main {
 				Score maxScore = Collections.max(K);
 			
 				for (Score score : K) {
-					int n = score.constestant;
-					if (score.score == maxScore.score)
-						ranks[n] = rank;
-					List<Double> sc;
-					if (scores.size() > 0)
-						sc = new ArrayList<>(scores.get(n));
-					else
-						sc = new ArrayList<>();
-					sc.add(score.score);
-					scores.get(n).add(Utils.mean(sc));
+					int n = score.contestant;
+					
+					// Rank is assigned if this was
+					// the contestant's last round
+					if (score == maxScore) ranks.set(n, rank);
+					
+					// Add the newest score to the history of contestant n
+					List<Double> scores_of_n = scores.get(n);
+					scores_of_n.add(score.score);
+					// REPLACE it by the average so far (to have a more "stable" number)
+					scores_of_n.set(scores_of_n.size()-1, Utils.mean(scores_of_n));
 				}
-				
-				// Next rank to be assigned
-				rank = 0;
-				for (int e : ranks)
-					if (e == 0) rank++;
 			}
 			
 		}
 		
 		System.out.println("Final ranking:");
-		for (int rank = 1; rank <= ranks.length; ++rank) {
+		for (int rank = 1; rank <= ranks.size(); ++rank) {
 			System.out.println("Rank " + rank);
-			for (int n = 0; n != ranks.length; ++n) {
-				if (ranks[n] != rank) continue;
+			// Show the strategies whose rank is rank
+			for (int n = 0; n != ranks.size(); ++n) {
+				if (ranks.get(n) != rank) continue;
 				System.out.println("   " + strategies.get(n).getName());
 			}
 		}
