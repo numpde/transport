7b2e547e5d2b3af7325c54fa813c6da93216fd5b
RA <>
2018-10-31 22:25:31 +0800

    RA: Large sleep time; JSON format info

--- pt2pt/20181019-study1/12_realtimelog.sh
+++ pt2pt/20181019-study1/12_realtimelog.sh
@@ -35,7 +35,9 @@ while true; do
 		> $f
 
 	echo ----
-	sleep 60
+
+	# The API data is not updated very frequently
+	sleep 100
 
 done
 
@@ -69,4 +71,94 @@ function l() {
     o = new SwaggerClient.ApiKeyAuthorization("x-date", r, "header");
   window.swaggerUi.api.clientAuthorizations.add("Authorization", e);
   window.swaggerUi.api.clientAuthorizations.add("x-date", o)
+}
+
+
+# JSON format of the response:
+
+BusA1Data {
+
+    PlateNumb (string):
+    車牌號碼 ,
+
+    OperatorID (string, optional):
+    營運業者代碼 ,
+
+    RouteUID (string, optional):
+    路線唯一識別代碼，規則為 {業管機關簡碼} + {RouteID}，其中 {業管機關簡碼} 可於Authority API中的AuthorityCode欄位查詢 ,
+
+    RouteID (string, optional):
+    地區既用中之路線代碼(為原資料內碼) ,
+
+    RouteName (NameType, optional):
+    路線名稱 ,
+
+    SubRouteUID (string, optional):
+    子路線唯一識別代碼，規則為 {業管機關簡碼} + {SubRouteID}，其中 {業管機關簡碼} 可於Authority API中的AuthorityCode欄位查詢 ,
+
+    SubRouteID (string, optional):
+    地區既用中之子路線代碼(為原資料內碼) ,
+
+    SubRouteName (NameType, optional):
+    子路線名稱 ,
+
+    Direction (string, optional):
+    去返程 = ['0: 去程', '1: 返程'],
+
+    BusPosition (PointType, optional):
+    車輛位置經度 ,
+
+    Speed (number, optional):
+    行駛速度(kph) ,
+
+    Azimuth (number, optional):
+    方位角 ,
+
+    DutyStatus (string, optional):
+    勤務狀態 = ['0: 正常', '1: 開始', '2: 結束'],
+
+    BusStatus (string, optional):
+    行車狀況 = ['0: 正常', '1: 車禍', '2: 故障', '3: 塞車', '4: 緊急求援', '5: 加油', '90: 不明', '91: 去回不明', '98: 偏移路線', '99: 非營運狀態', '100: 客滿', '101: 包車出租', '255: 未知'],
+
+    MessageType (string, optional):
+    資料型態種類 = ['0: 未知', '1: 定期', '2: 非定期'],
+
+    GPSTime (DateTime):
+    車機時間(ISO8601格式:yyyy-MM-ddTHH:mm:sszzz) ,
+
+    TransTime (DateTime, optional):
+    車機資料傳輸時間(ISO8601格式:yyyy-MM-ddTHH:mm:sszzz)[多數單位沒有提供此欄位資訊] ,
+
+    SrcRecTime (DateTime, optional):
+    來源端平台接收時間(ISO8601格式:yyyy-MM-ddTHH:mm:sszzz) ,
+
+    SrcTransTime (DateTime, optional):
+    來源端平台資料傳出時間(ISO8601格式:yyyy-MM-ddTHH:mm:sszzz)[公總使用TCP動態即時推播故有提供此欄位, 而非公總系統因使用整包資料更新, 故沒有提供此欄位] ,
+
+    SrcUpdateTime (DateTime, optional):
+    來源端平台資料更新時間(ISO8601格式:yyyy-MM-ddTHH:mm:sszzz)[公總使用TCP動態即時推播故沒有提供此欄位, 而非公總系統因提供整包資料更新, 故有提供此欄] ,
+
+    UpdateTime (DateTime):
+    本平台資料更新時間(ISO8601格式:yyyy-MM-ddTHH:mm:sszzz)
+
+}
+
+NameType {
+
+    Zh_tw (string, optional):
+    中文繁體名稱 ,
+
+    En (string, optional):
+    英文名稱
+
+}
+
+PointType {
+
+    PositionLat (number, optional):
+    位置緯度(WGS84) ,
+
+    PositionLon (number, optional):
+    位置經度(WGS84)
+
 }
