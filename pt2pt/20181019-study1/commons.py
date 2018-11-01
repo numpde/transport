31fc0b5e77a855344a7090e065f85006b15dfed3
RA <>
2018-11-02 02:34:16 +0800

    RA: Common methods

---
+++ pt2pt/20181019-study1/commons.py
@@ -0,0 +1,124 @@
+
+# RA, 2018-11-01
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Print which files are opened
+def logged_open(filename, mode='r', *argv, **kwargs) :
+	print("({}):\t{}".format(mode, filename))
+	return open(filename, mode, *argv, **kwargs)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+from collections import defaultdict
+
+# Index an iterable _I_ of dict's by the return value of key_func
+def index_dicts_by_key(I, key_func) :
+	J = defaultdict(lambda: defaultdict(list))
+
+	for i in I :
+		for (k, v) in i.items() :
+			J[key_func(i)][k].append(v)
+
+	for (j, i) in J.items() :
+		for (k, V) in i.items() :
+			if (1 == len(set(json.dumps(v) for v in V))) :
+				J[j][k] = next(iter(V))
+
+	# Convert all defaultdict to dict
+	J = json.loads(json.dumps(J))
+
+	return J
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Does the collection L contain mutually distinct elements?
+def is_distinct(L) :
+	return (len(set(L)) == len(list(L)))
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import sys
+
+def parse_options(OPTIONS) :
+
+	if (len(sys.argv) > 1) :
+		(OPT, ARG) = (sys.argv[1], sys.argv[2:])
+		for (opt, fun) in OPTIONS.items() :
+			if (opt == OPT) :
+				fun(*ARG)
+				return True
+
+		raise RuntimeError("Unrecognized command line option")
+
+	return False
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import json, zlib, base64
+
+class ZIPJSON :
+
+	TAGS = ['base64(zip(o))', 'base64zip']
+
+	def __init__(self, data) :
+
+		try :
+			json.dumps(data)
+		except :
+			raise RuntimeError("The passed object not recognized as JSON")
+
+		self.data = data
+
+	def enc(self):
+
+		if not self.data : return self.data
+
+		if (type(self.data) is dict) :
+			if (1 == len(self.data)) :
+				if set.intersection(set(self.data.keys()), set(self.TAGS)) :
+					print(self.data)
+					raise RuntimeError("It appears that the input is compressed already")
+
+		C = {
+			self.TAGS[0] : base64.b64encode(
+				zlib.compress(
+					json.dumps(self.data).encode('utf-8')
+				)
+			).decode('ascii')
+		}
+
+		return C
+
+	def try_dec(self) :
+
+		if not (type(self.data) is dict) : return self.data
+
+		def dec(D) :
+			for (k, v) in D.items() :
+				if k in self.TAGS :
+					v = json.loads( zlib.decompress( base64.b64decode( v ) ) )
+				yield (k, v)
+
+		J = dict(dec(self.data))
+
+		if (1 == len(J)) :
+			if set.intersection(set(J.keys()), set(self.TAGS)) :
+				(_, J) = J.popitem()
+
+		return J
+
+def zipjson_load(fn) :
+	assert(type(fn) is str)
+	J = json.load(open(fn, 'r'))
+	J = ZIPJSON(J).try_dec()
+	return J
+
+def zipjson_dump(J, fn) :
+	assert(type(fn) is str)
+	E = ZIPJSON(J).enc()
+	assert(json.dumps(E))
+	return json.dump(E, open(fn, 'w'))
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
