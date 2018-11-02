c7c716146b661721830aaf3c9df479d6898abb41
RA <>
2018-11-02 21:08:29 +0800

    RA: Correction in "listify"

--- pt2pt/20181019-study1/13_physicalbus.py
+++ pt2pt/20181019-study1/13_physicalbus.py
@@ -51,7 +51,7 @@ THIS = inspect.getsource(inspect.getmodule(inspect.currentframe()))
 
 def extract_busses() :
 
-	response_files = sorted(glob.glob(IFILE['response'].format(d="20181102", t="12*")))
+	response_files = sorted(glob.glob(IFILE['response'].format(d="20181102", t="1*")))
 	time.sleep(1)
 
 	keys = {
@@ -72,14 +72,23 @@ def extract_busses() :
 		busid_of
 	)
 
+	i = 'KOffice_test'
+	B = { i : B[i] }
+
 	print("Found {} physical busses".format(len(B)))
 
-	# Make a single-element list, if it is not a list already
-	def listify(V):
-		return V if (type(V) is list) else ([V] * len(b[keys['time']]))
+	for b in B:
+		print(b)
 
 	# Undo index_dicts_by_key on an element
 	def follow(b) :
+
+		# In case of a non-list, make an iterable returning this element
+		def listify(V):
+			if type(V) is list:
+				return V
+			else:
+				while True: yield V
 
 		# TODO: the following does not hold
 		#assert(commons.is_distinct(b[key_time]))
@@ -96,12 +105,20 @@ def extract_busses() :
 		L = []
 
 		for b in bb :
+			# if (busid_of(b) == 'KOffice_test') :
+			# 	print(b)
 			if not L :
 				# First record of the new segment
 				L.append(b)
 			elif all((b[k] == L[-1][k]) for k in indicators) :
-				# Continue segment record
+				# None of the indicators have changed, so
+				# continue the segment record
 				# TODO: Segment if delta(GPSTime) is large?
+
+				# (unless there is no change at all, then do nothing)
+				if (b == L[-1]) :
+					continue
+
 				L.append(b)
 			else :
 				# Sanity check: only one bus tracked in the segment
@@ -122,18 +139,24 @@ def extract_busses() :
 		for s in segments(follow(b)) :
 			# Here, the segment s looks like
 			# {'PlateNumb': '756-V2', 'SubRouteUID': 'KHH912', 'Direction': 1, 'GPSTime': [List of time stamps], 'BusPosition': [List of positions]}
+			print(s)
 			pass
 
+	exit(39)
+
 	keys_pos = {
-		'Lat'     : 'PositionLat',
-		'Lon'     : 'PositionLon',
+		'Lat' : 'PositionLat',
+		'Lon' : 'PositionLon',
 	}
 
 	# Function to compress the list of positions for a segment
 	def unwrap_pos(s) :
+		if not (type(s[keys['pos']]) is list) :
+			s[keys['pos']] = list(s[keys['pos']])
+
 		(s[keys_pos['Lat']], s[keys_pos['Lon']]) = (
-			list(c) for c in
-			zip(*[ tuple(bp[k] for k in keys_pos.values()) for bp in listify(s[keys['pos']]) ])
+			list(coo) for coo in
+			zip(*[ tuple(bp[k] for k in keys_pos.values()) for bp in s[keys['pos']] ])
 		)
 		del s[keys['pos']]
 
