823fec0447c1811dc11fa230ce92111f251dea2e
RA <>
2018-11-03 22:14:10 +0800

    RA: Show all recorded runs of a given subroute

--- pt2pt/20181019-study1/16_realtimevis.py
+++ pt2pt/20181019-study1/16_realtimevis.py
@@ -11,6 +11,7 @@ import json
 import glob
 import inspect
 from itertools import chain
+from collections import defaultdict
 import matplotlib.pyplot as plt
 
 
@@ -57,10 +58,16 @@ def vis1() :
 
 	route_stops = commons.index_dicts_by_key(commons.zipjson_load(IFILE['route-stops']), (lambda r: r['SubRouteUID']))
 
-	bus_files = sorted(glob.glob(IFILE['busses'].format(busid="*")))
-	fn = bus_files[1]
+	runs_by_route = defaultdict(list)
 
-	runs = commons.zipjson_load(fn)
+	bus_files = sorted(glob.glob(IFILE['busses'].format(busid="*")))
+
+	for fn in bus_files :
+		runs = commons.zipjson_load(fn)
+		for run in runs :
+			runs_by_route[run['SubRouteUID']].append(run)
+
+	runs = runs_by_route['122']
 
 	for run in runs :
 
@@ -71,6 +78,8 @@ def vis1() :
 		route = route_stops[route_uid]
 		stops = dict(zip(route['Direction'], route['Stops']))[run['Direction']]
 
+		print(stops)
+
 		# Clear figure
 		plt.clf()
 
@@ -78,11 +87,11 @@ def vis1() :
 		for stop in stops :
 			p = stop['StopPosition']
 			(y, x) = (p['PositionLat'], p['PositionLon'])
-			plt.scatter(x, y, c='b', marker='o' )
+			plt.scatter(x, y, c='b', marker='o')
 
 		# Trace bus
 		(y, x) = (run['PositionLat'], run['PositionLon'])
-		plt.plot(x, y, '-', c='r', linewidth=1)
+		plt.plot(x, y, '--+', c='r', linewidth=1)
 
 		plt.draw(); plt.pause(0.1)
 
