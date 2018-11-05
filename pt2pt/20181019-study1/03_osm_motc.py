237ea3e5fa651c349a16a5afa2e6826ab2e6232d
RA <>
2018-11-06 02:13:49 +0800

    RA: Impl interactive bus stop/route lookup

--- pt2pt/20181019-study1/03_osm_motc.py
+++ pt2pt/20181019-study1/03_osm_motc.py
@@ -9,6 +9,7 @@ import commons
 import re
 import difflib
 import pickle
+import json
 import glob
 import time
 import inspect
@@ -26,7 +27,8 @@ pass
 
 IFILE = {
 	'OSM'  : "OUTPUT/02/UV/kaohsiung.pkl",
-	'MOTC' : "ORIGINALS/MOTC/Kaohsiung/CityBusApi_StopOfRoute/data.json",
+	'MOTC_routes' : "ORIGINALS/MOTC/Kaohsiung/CityBusApi_StopOfRoute/data.json",
+	'MOTC_stops' : "ORIGINALS/MOTC/Kaohsiung/CityBusApi_Stop/data.json",
 }
 
 
@@ -46,6 +48,9 @@ PARAM = {
 # https://stackoverflow.com/questions/34491808/how-to-get-the-current-scripts-code-in-python
 THIS = inspect.getsource(inspect.getmodule(inspect.currentframe()))
 
+# Print a JSON nicely
+def pretty_print(J) :
+	print(json.dumps(J, indent=2, ensure_ascii=False))
 
 ## ===================== WORK :
 
@@ -128,28 +133,170 @@ def check_osm_routes() :
 		assert(route_tags['type'] in ['route', 'route_master'])
 
 		print("")
-		print("Checking route #{} '{}':".format(route_id, route_name), route)
+		print("Checking route #{} '{}':".format(route_id, route_name))
 		print("")
 
-		assert(route_tags.get('type') == 'route'), "(mandatory)"
-		assert(route_tags.get('route') == 'bus'), "(mandatory)"
+		if not (route_tags.get('type') == 'route') :
+			print("ISSUE: tag type=route not found")
+			print(route)
+			input("Press ENTER")
+      
+		if not (route_tags.get('route') == 'bus') :
+			print("ISSUE: tag route=bus not found")
+			print(route)
+			input("Press ENTER")
 
-		assert(route_tags.get('ref')), "Very important"
+		if not route_tags.get('ref') :
+			print("ISSUE: tag 'ref' not found")
+			print(route)
+			input("Press ENTER")
 
-		# assert(route_tags.get('roundtrip') in ['yes', 'no']), "Important"
+		if not (route_tags.get('roundtrip') in ['yes', 'no']) :
+			print("ISSUE: tag roundtrip=yes/no not found")
+			print(route)
+			input("Press ENTER")
 	
-		assert(route_tags.get('public_transport:version') == '2'), "Important"
+		if not (route_tags.get('public_transport:version') == '2') :
+			print("ISSUE: tag public_transport:version=2 not found")
+			print(route)
+			input("Press ENTER")
 
-		assert(route_tags.get('name')), "Recommended"
+		if not route_tags.get('name') :
+			print("ISSUE: tag 'name' not found")
+			print(route)
+			input("Press ENTER")
 
-		#print(route_id
+
+# Look for bus stops or bus routes base on user input
+def interactive_match() :
+
+	motc_stops = commons.index_dicts_by_key(commons.zipjson_load(IFILE['MOTC_stops']), (lambda r: r['StopUID']))
+	motc_routes = commons.index_dicts_by_key(commons.zipjson_load(IFILE['MOTC_routes']), (lambda r: r['SubRouteUID']))
+
+	def strip_brackets(s):
+		return re.match(r'(?P<name>[^\(]+)[ ]*(?P<extra>\(\w+\))*', s).group('name').strip()
+
+	def matchratio_names(name1, name2):
+		return difflib.SequenceMatcher(None, name1, name2).ratio()
+
+	def matchratio(query, keywords) :
+		return max([matchratio_names(query.lower(), keyword.lower()) for keyword in keywords])
+
+	# Search keywords for bus stops
+	motc_s_kw = {
+		j : [
+			stop[k]
+			for k in ['StopUID', 'StopID', 'StationID']
+		] + [
+				str(coo)
+				for coo in stop['StopPosition'].values()
+		] + [
+			strip_brackets(name)
+			for name in stop['StopName'].values()
+		]
+		for (j, stop) in motc_stops.items()
+	}
+
+	# Search keywords for bus routes
+	motc_r_kw = {
+		j : [
+			route[k]
+			for k in ['RouteUID', 'RouteID', 'SubRouteUID', 'SubRouteID']
+		] + [
+			strip_brackets(name)
+			for name in route['SubRouteName'].values()
+		]
+		for (j, route) in motc_routes.items()
+	}
+
+	# Interaction
+
+	q = input("\nEnter command (s, S, r, R):\n").strip()
+	if not q : return
+
+	q = q.split(' ')
+
+	if (len(q) < 2) :
+		interactive_match()
+		return
+
+	(query_type, query) = (q[0], " ".join(q[1:]))
+
+	def slim_stop(stop) :
+		return {
+			k : stop[k]
+			for k in ['StopUID', 'StopName']
+		}
+
+	def slim_route(route) :
+		return {
+			k : route[k]
+			for k in ['SubRouteUID', 'SubRouteName']
+		}
+
+
+	# Search for bus stops
+	if (query_type.lower() == "s") :
+
+		top_match_motc = sorted(
+			motc_stops.items(),
+			key=(lambda item : matchratio(query, motc_s_kw[item[0]])),
+			reverse=True
+		)[0:25]
+
+		if (query_type == 's') :
+
+			print("Suggestions:")
+
+			for (_, stop) in top_match_motc :
+				print(slim_stop(stop))
+
+		else :
+
+			(_, stop) = top_match_motc[0]
+			pretty_print(stop)
+
+	# Search for bus routes
+	if (query_type.lower() == "r"):
+
+		top_match_motc = sorted(
+			motc_routes.items(),
+			key=(lambda item : matchratio(query, motc_r_kw[item[0]])),
+			reverse=True
+		)[0:10]
+
+		if (query_type == 'r') :
+
+			print("Suggestions:")
+
+			for (route_id, route) in top_match_motc :
+				print(slim_route(route))
+
+		else :
+
+			(route_id, route) = top_match_motc[0]
+
+			stops = route['Stops']
+			del route['Stops']
+
+			pretty_print(route)
+
+			def zip_listify(a, b):
+				return zip(a, b) if (type(a) is list) else zip([a], [b])
+
+			for (dir, stops) in zip_listify(route['Direction'], stops) :
+				print("Direction", dir)
+				pretty_print(stops)
+
+	interactive_match()
+	return
 
 ## ===================== PLAY :
 
 def match_routes() :
 
 	# MOTC route info
-	motc_routes = commons.index_dicts_by_key(commons.zipjson_load(IFILE['MOTC']), (lambda r: r['SubRouteUID']))
+	motc_routes = commons.index_dicts_by_key(commons.zipjson_load(IFILE['MOTC_routes']), (lambda r: r['SubRouteUID']))
 
 	# for (route_id, route) in route_stops.items() :
 	# 	stops = dict(zip(route['Direction'], route['Stops']))
@@ -267,8 +414,9 @@ def match_routes() :
 
 OPTIONS = {
 	# 'CHECK_STOPS' : check_osm_stops,
-	'CHECK_ROUTES' : check_osm_routes,
+	# 'CHECK_ROUTES' : check_osm_routes,
 	# 'MATCH' : match_routes,
+	'INTERACTIVE' : interactive_match,
 }
 
 ## ==================== ENTRY :
