745ced0c97c1661d8fdbdfa1fce69bfce9fef312
RA <>
2018-11-13 09:28:30 +0800

    RA: Small maps for testing purposes

---
+++ pt2pt/20181019-study1/ORIGINALS/OSM/readme.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# This folder contains a collection of small maps for testing purposes
+
+# Download Taipei 101 neighborhood map
+curl "https://overpass-api.de/api/map?bbox=121.5560,25.0245,121.5746,25.0409" -o TP101.osm
+
+
