3ea782d6481ecd6e3d6e78f203e5d59f56a2825e
r <romanize@hotmail.com>
2018-12-12 10:46:13 +0800

    RA: Exception handling if no internet connection

--- pt2pt/20181019-study1/helpers/maps.py
+++ pt2pt/20181019-study1/helpers/maps.py
@@ -2,12 +2,15 @@
 # RA, 2018-11-07
 
 import io
+
 from enum import Enum
 from helpers import commons
 
 from PIL import Image
 
 from math import pi, log, tan, exp, atan
+
+from urllib.error import URLError
 
 import matplotlib as mpl
 
@@ -140,7 +143,7 @@ def write_track_img(waypoints, tracks, fd, mapbox_api_token=None, plotter=None, 
 
 		if waypoints :
 			for (y, x) in waypoints :
-				ax.plot(x, y, 'o', alpha=0.3, c='b', markersize=0.3)
+				ax.plot(x, y, 'o', alpha=0.3, c='k', markersize=0.3)
 
 	(plotter or default_plotter)(fig, ax)
 
@@ -148,8 +151,11 @@ def write_track_img(waypoints, tracks, fd, mapbox_api_token=None, plotter=None, 
 	[i.set_fontsize(8) for i in ax.get_xticklabels() + ax.get_yticklabels()]
 	ax.axis(axis)
 
-	if mapbox_api_token :
-		ax.imshow(get_map_by_bbox(ax2mb(*axis), token=mapbox_api_token), extent=axis, interpolation='quadric', zorder=-100)
+	try :
+		if mapbox_api_token :
+			ax.imshow(get_map_by_bbox(ax2mb(*axis), token=mapbox_api_token), extent=axis, interpolation='quadric', zorder=-100)
+	except URLError as e:
+		print("Warning: no background map (no internet connection?)")
 
 	fig.savefig(fd, dpi=dpi, bbox_inches='tight', pad_inches=0)
 	plt.close(fig)
